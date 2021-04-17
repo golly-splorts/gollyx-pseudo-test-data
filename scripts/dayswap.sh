1413f77fe63c205917dc78c3c598b615e9513ae1
Ch4zm of Hellmouth <ch4zm.of.hellmouth@gmail.com>
2021-04-17 10:17:16 -0700

    add day swap script

---
+++ scripts/dayswap.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+gsed -i -e 's/"day": 49/"day": 9/g' $1
+gsed -i -e 's/"day": 50/"day": 10/g' $1
+gsed -i -e 's/"day": 51/"day": 11/g' $1
+gsed -i -e 's/"day": 52/"day": 12/g' $1
+gsed -i -e 's/"day": 53/"day": 13/g' $1
+
+gsed -i -e 's/"day": 54/"day": 14/g' $1
+gsed -i -e 's/"day": 55/"day": 15/g' $1
+gsed -i -e 's/"day": 56/"day": 16/g' $1
+gsed -i -e 's/"day": 57/"day": 17/g' $1
+gsed -i -e 's/"day": 58/"day": 18/g' $1
+
+gsed -i -e 's/"day": 59/"day": 19/g' $1
+gsed -i -e 's/"day": 60/"day": 20/g' $1
+gsed -i -e 's/"day": 61/"day": 21/g' $1
+gsed -i -e 's/"day": 62/"day": 22/g' $1
+gsed -i -e 's/"day": 63/"day": 23/g' $1
+gsed -i -e 's/"day": 64/"day": 24/g' $1
+gsed -i -e 's/"day": 65/"day": 25/g' $1
