6c436b9b51878b9fb6b5ac456e5e9af98d38fbc8
Ch4zm of Hellmouth <ch4zm.of.hellmouth@gmail.com>
2021-04-28 21:56:12 -0700

    update swap colors script to operate on more seasons

--- scripts/swap_colors.sh
+++ scripts/swap_colors.sh
@@ -2,12 +2,12 @@
 
 
 # Swap TB bb0000 with DECO e7a0e8
-gsed -i -e 's/bb0000/SENTINELSTRING/g' season{0,1,2}/*.json
-gsed -i -e 's/BB0000/SENTINELSTRING/g' season{0,1,2}/*.json
-gsed -i -e 's/e7a0e8/bb0000/g' season{0,1,2}/*.json
-gsed -i -e 's/SENTINELSTRING/e7a0e8/g' season{0,1,2}/*.json
+gsed -i -e 's/bb0000/SENTINELSTRING/g' season{3,4,5,6,7,8,9,10,11}/*.json
+gsed -i -e 's/BB0000/SENTINELSTRING/g' season{3,4,5,6,7,8,9,10,11}/*.json
+gsed -i -e 's/e7a0e8/bb0000/g' season{3,4,5,6,7,8,9,10,11}/*.json
+gsed -i -e 's/SENTINELSTRING/e7a0e8/g' season{3,4,5,6,7,8,9,10,11}/*.json
 
 # Swap DECO e7a0e8 with AA ff1717
-gsed -i -e 's/e7a0e8/SENTINELSTRING/g' season{0,1,2}/*.json
-gsed -i -e 's/ff1717/e7a0e8/g' season{0,1,2}/*.json
-gsed -i -e 's/SENTINELSTRING/ff1717/g' season{0,1,2}/*.json
+gsed -i -e 's/e7a0e8/SENTINELSTRING/g' season{3,4,5,6,7,8,9,10,11}/*.json
+gsed -i -e 's/ff1717/e7a0e8/g' season{3,4,5,6,7,8,9,10,11}/*.json
+gsed -i -e 's/SENTINELSTRING/ff1717/g' season{3,4,5,6,7,8,9,10,11}/*.json
